54292b3827561f61bd7978bb0e2f790ebdb4c7a2
TenViki <viktorkomarek28@gmail.com>
2022-03-17 16:19:30 +0100

    :sparkles: Adds route for uploading files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
+import { FilesInterceptor } from "@nestjs/platform-express";
 
-@Controller('files')
-export class FilesController {}
+@Controller("files")
+export class FilesController {
+  @Post()
+  @UseInterceptors(FilesInterceptor("files"))
+  uploadFile(@UploadedFiles() files: Express.Multer.File[]) {
+    return files;
+  }
+}
