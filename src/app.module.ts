93f5480ebc8ec3f886a3e6b8abd0bcaf417416f1
TenViki <viktorkomarek28@gmail.com>
2022-03-17 16:04:24 +0100

    :sparkles: Adds database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from "@nestjs/common";
-import { FilesModule } from './files/files.module';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { FileEntity } from "./entities/file.entity";
+import { Note } from "./entities/note.entity";
+import { Tag } from "./entities/tag.entity";
+import { FilesModule } from "./files/files.module";
 
 @Module({
-  imports: [FilesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [FileEntity, Tag, Note],
+      synchronize: process.env.NODE_ENV !== "production",
+    }),
+    FilesModule,
+  ],
   controllers: [],
   providers: [],
 })
