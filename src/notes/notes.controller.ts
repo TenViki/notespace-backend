810dcf710d09355535fd1ddd6c0e40b4dc7d0a07
TenViki <viktorkomarek28@gmail.com>
2022-03-17 21:51:53 +0100

    :sparkles: Adds route for creating ntoe

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,8 +1,13 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { NewNoteDto } from "./new-note.dto";
+import { NotesService } from "./notes.service";
 
 @Controller("notes")
 export class NotesController {
+  constructor(private noteService: NotesService) {}
+
   @Post("/")
-  async createNote(@Body() note: NewNoteDto) {}
+  async createNote(@Body() note: NewNoteDto) {
+    return this.noteService.createNote(note);
+  }
 }
